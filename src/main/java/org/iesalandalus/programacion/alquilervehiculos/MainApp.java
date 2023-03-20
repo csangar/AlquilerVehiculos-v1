4cd1a55a32f96ef24880fc2668fa560fcf4306cd
csangar <crisangar98@gmail.com>
2023-03-20 13:12:11 +0100

    Algunos refinamientos de codigo

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -6,6 +6,7 @@ import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
+import org.iesalandalus.programacion.alquilervehiculos.vista.FactoriaVista;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 import org.iesalandalus.programacion.alquilervehiculos.vista.texto.VistaTexto;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
@@ -14,8 +15,7 @@ public class MainApp {
 
 	public static void main(String[] args) throws OperationNotSupportedException {
 		// Ánimo!!!!
-		Vista vista = new VistaTexto();
-		//Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
+		Vista vista =FactoriaVista.TEXTO.crear();
 		Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
 		Controlador controlador = new Controlador(vista, modelo);
 		controlador.comenzar();
